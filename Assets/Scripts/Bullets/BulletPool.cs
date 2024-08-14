ccb0471262a0b27ade544d4340234ff2ac093045
Sega-13 <segapatidar@gmail.com>
2024-08-14 12:25:09 +0530

    Implemented GetBullet method in BulletPool class.

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Analytics;
 using UnityEngine;
+using static CosmicCuration.Bullets.BulletPool;
 
 namespace CosmicCuration.Bullets
 {
@@ -13,6 +15,26 @@ namespace CosmicCuration.Bullets
         { 
             this.bulletView = bulletView;
             this.bulletScriptableObject = bulletScriptableObject;
+        }
+        public BulletController GetBullet()
+        {
+            if (pooledbullet.Count > 0)
+            {
+                PooledBullet pool = pooledbullet.Find(item => !item.isUsed);
+                if (pool != null)
+                {
+                    pool.isUsed = true;
+                    return pool.Bullet;
+                }
+            }            
+            return CreateNewPooledBullet();
+        }
+        private BulletController CreateNewPooledBullet()
+        {
+            PooledBullet pooledBullet = new PooledBullet();
+            pooledBullet.Bullet = new BulletController(bulletView, bulletScriptableObject);
+            pooledBullet.isUsed = true;
+            return pooledBullet.Bullet;
         }
         public class PooledBullet
         {
